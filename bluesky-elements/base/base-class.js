9763df9dc757e6f5f0539ec504070f9a69d3e68f
Alex Rafter~ <alexrafteremail@gmail.com>
2023-02-18 13:49:20 +0000

    added working importmap for petite

--- bluesky-elements/base/base-class.js
+++ bluesky-elements/base/base-class.js
@@ -1,4 +1,4 @@
-import { createApp } from "https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js";
+import { createApp } from "petite-vue";
 
 export class baseEl extends HTMLElement {
   constructor() {
